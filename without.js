db32a16175c1675652f868540f3d92e760293b4f
Zahra Ardalani <zahra.ardalani@gmail.com>
2020-08-18 23:23:40 +0000

    without added

---
+++ without.js
@@ -0,0 +1,34 @@
+let eqArrays = function(arr1, arr2){
+  if(arr1.length !== arr2.length){
+    return false;
+  } else {
+    for(let i=0; i< arr1.length; i++){
+      if(arr1[i] !== arr2[i]){
+        return false;
+      }
+    }
+    return true;
+  }
+}
+
+let assertArraysEqual = function(arr1, arr2){
+  let failEmoji = String.fromCodePoint(0x1F534);
+  let passEmoji = String.fromCodePoint(0x2705);
+  if(eqArrays(arr1, arr2)){
+    return `${passEmoji}${passEmoji}${passEmoji} Arrays are equal.`
+  } else {
+    return `${failEmoji}${failEmoji}${failEmoji} Arrays are not equal.`
+  }
+}
+
+let without = function(source, itemsToRemove){
+  let arr = source.slice(0);
+  for (let i=0; i< arr.length; i++){
+    for (let item of itemsToRemove){
+      if(item === arr[i]){
+        arr.splice(i, 1);
+      }
+    }
+  }
+  return arr;
+}
