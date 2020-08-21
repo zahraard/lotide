b8f56d8f6db66dafe1ef616b6d6e76bdb8cb0f54
Zahra Ardalani <zahra.ardalani@gmail.com>
2020-08-21 00:51:15 +0000

    take until added

---
+++ takeUntil.js
@@ -0,0 +1,18 @@
+const takeUntil = function(array, callback) {
+  const result = [];
+  for(item of array){
+    if(!callback(item)){
+      result.push(item)
+    } else return result;
+  }
+  return result;
+}
+
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
